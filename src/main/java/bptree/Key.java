cce23c10669211a58d9d5d7986facbba30b98f31
Max Sumrall <jmsumrall@gmail.com>
2015-04-10 15:59:33 +0200

    Improved cohesion of the Key and the implementing class.
    Provided a method for building the composed form of the key, with the pathID of the label path.

--- src/main/java/bptree/Key.java
+++ src/main/java/bptree/Key.java
@@ -1,63 +1,12 @@
 package bptree;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 /**
- * Implements Comparator for sorting keys.
+ * Created by max on 3/26/15.
  */
-public class Key implements Comparator<Long[]> {
+public interface Key{
 
-    /**
-     * Given two long[]'s, which represent the keys, compare them.
-     * @param a key
-     * @param b key
-     * @return a negative integer, zero, or a positive integer if the first argument is less than, equal to, or greater than the second.
-     */
-    public int compare(Long[] a, Long[] b) {
+    Long[] getLabelPath();
 
-        if(a.length != b.length){ return a.length - b.length; }
-        long temp;
-        for (int i = 0; i < a.length; i++) {
-            temp = a[i] - b[i];
-            if (temp != 0) {
-                return a[i].compareTo(b[i]);
-            }
-        }
-        return 0;
-    }
-    public int prefixCompare(Long[] search_key, Long[] key) {
-        long temp;
-        for (int i = 0; i < search_key.length; i++) {
-            temp = search_key[i] - key[i];
-            if (temp != 0) {
-                return search_key[i].compareTo(key[i]);
-            }
-        }
-        return 0;
-    }
+    Long[] getComposedKey(Long pathID);
 
-    /**
-     * Given to keys a, b, determines if a is a valid prefix of b.
-     * @param a prefix to check. Your search key
-     * @param b full key to compare to. Your value in the index.
-     * @return
-     */
-    public boolean validPrefix(Long[] a, Long[] b){
-
-        if((a.length > b.length) || a.length == 0){ //The empty prefix should match nothing. You must specify atleast the path, so give me at least one item.
-            return false;
-        }
-        for(int i = 0; i < a.length; i++){
-            if(!a[i].equals(b[i])){
-                return false;
-            }
-        }
-        return true;
-
-    }
-
-    public static String asString(Long[] key){
-        return Arrays.toString(key);
-    }
 }
