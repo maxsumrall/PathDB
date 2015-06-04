ec7bd1837c46c24d511c55147a1b8809dc3b39a9
Max Sumrall <jmsumrall@gmail.com>
2015-06-04 10:22:09 +0200

    editing pom

---
+++ src/main/java/NeoIntegration/Runner.java
@@ -0,0 +1,14 @@
+package NeoIntegration;
+
+import java.io.IOException;
+
+public class Runner {
+    public static void main(String[] args) throws IOException {
+        if(args[0].equals("1")){
+            BulkLUBMDataLoader.main(args);
+        }
+        else if (args[0].equals("2")){
+            CleverIndexBuilder.main(args);
+        }
+    }
+}
