f6a39be430aff8bc17d7a7a893979680d99f0d4a
Max Sumrall <max.sumrall@neotechnology.com>
2016-09-18 20:33:31 +0200

    Restructure a few modules. Created a new sibling project.
    Some of the content was specific for the project, not
    for sasifying the needs of this data store. Things that I find
     not necessary for this work will be moved over there.

---
+++ src/main/java/PathIndex/CompressedPageCursor.java
@@ -0,0 +1,383 @@
+/**
+ * Copyright (C) 2015-2016 - All rights reserved.
+ * This file is part of the PathDB project which is released under the GPLv3 license.
+ * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl.txt for full license details.
+ * You may use, distribute and modify this code under the terms of the GPLv3 license.
+ */
+
+package PathIndex;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+import org.neo4j.io.pagecache.PageCursor;
+
+
+public class CompressedPageCursor extends PageProxyCursor{
+    PageCursor cursor;
+    int maxPageSize = DiskCache.PAGE_SIZE * 15;
+    ByteBuffer dBuffer = ByteBuffer.allocate(maxPageSize);
+    int mostRecentCompressedLeafSize = DiskCache.PAGE_SIZE;//the default value
+    boolean deferWriting = false;
+    int maxNumBytes;
+    final int sameID = 128;
+    final int sameFirstNode = 64;
+
+    public CompressedPageCursor(DiskCache disk, long pageId, int lock) throws IOException {
+        this.cursor = disk.pagedFile.io(pageId, lock);
+        cursor.next();
+        loadCursorFromDisk();
+    }
+
+    @Override
+    public void next(long page) throws IOException {
+        cursor.next(page);
+        loadCursorFromDisk();
+        dBuffer.position(0);
+    }
+
+    public void pushChangesToDisk(){
+        if(!deferWriting) {
+            int mark = dBuffer.position();
+            if (NodeHeader.isLeafNode(dBuffer))
+                compressAndWriteLeaf();
+            else
+                writeInternal();
+            dBuffer.position(mark);
+        }
+    }
+
+    private void writeInternal(){
+        dBuffer.position(0);
+        cursor.setOffset(0);
+        for(int i = 0; i < DiskCache.PAGE_SIZE; i++){
+            cursor.putByte(dBuffer.get());
+        }
+    }
+
+
+    private void compressAndWriteLeaf(){
+        //compress the contents of dBuffer. Must first determine how big dBuffer actually is.
+        //assumption that this is a leaf node.
+        //will just check if the path id is zero, if so, this is the end of this block.
+        int decompressedSize = getLastUsedLeafBufferPosition() - NodeHeader.NODE_HEADER_LENGTH;
+        writeHeaderToCursor();
+        if(decompressedSize != 0) {
+            byte[] compresedMinusHeader = compress();
+            cursor.setOffset(NodeHeader.NODE_HEADER_LENGTH);
+            cursor.putBytes(compresedMinusHeader);
+            mostRecentCompressedLeafSize = compresedMinusHeader.length + NodeHeader.NODE_HEADER_LENGTH;
+        }
+    }
+
+    public byte[] compress(){
+        int keyLength = NodeHeader.getKeyLength(cursor);
+        int numberOfKeys = NodeHeader.getNumberOfKeys(cursor);
+        ByteBuffer buffer = ByteBuffer.allocate(DiskCache.PAGE_SIZE - NodeHeader.NODE_HEADER_LENGTH);
+        long[] next = new long[keyLength];
+        long[] prev = new long[keyLength];
+        dBuffer.position(NodeHeader.NODE_HEADER_LENGTH);
+        for(int i = 0; i < numberOfKeys; i++) {
+            for (int j = 0; j < keyLength; j++) {
+                next[j] = dBuffer.getLong();
+            }
+            buffer.put(encodeKey(next, prev));
+            prev = next;
+        }
+        byte[] truncatedCompressed = new byte[buffer.position()];
+        System.arraycopy(buffer.array(), 0, truncatedCompressed, 0, truncatedCompressed.length);
+        return truncatedCompressed;
+    }
+
+    public byte[] encodeKey(long[] key, long[] prev){
+
+        this.maxNumBytes = 0;
+        int firstEncodedIndex = 0;
+        byte header = (byte)0;
+        if(key[0] == prev[0]) {
+            //set first bit
+            firstEncodedIndex++;
+            header |= sameID;
+
+            if (key[1] == prev[1]) {
+                //set second bit
+                firstEncodedIndex++;
+                header |= sameFirstNode;
+            }
+        }
+
+        for(int i = firstEncodedIndex; i < key.length; i++){
+            maxNumBytes = Math.max(maxNumBytes, numberOfBytes(key[i] - prev[i]));
+        }
+
+        byte[] encoded = new byte[1 + (maxNumBytes * (key.length - firstEncodedIndex))];
+        header |=  maxNumBytes;
+        encoded[0] = header;
+        for(int i = 0; i < key.length - firstEncodedIndex; i++){
+            toBytes(key[i + firstEncodedIndex] - prev[i + firstEncodedIndex], encoded, 1 + (i * maxNumBytes), maxNumBytes);
+        }
+        return encoded;
+    }
+
+    public static int numberOfBytes(long value){
+        long abs = Math.abs(value);
+        int minBytes = 8;
+        if(abs <= 127){
+            minBytes = 1;
+        }
+        else if(abs <= 32768){
+            minBytes = 2;
+        }
+        else if(abs <= 8388608){
+            minBytes = 3;
+        }
+        else if(abs <= 2147483648l){
+            minBytes = 4;
+        }
+        else if(abs <= 549755813888l){
+            minBytes = 5;
+        }
+        else if(abs <= 140737488355328l){
+            minBytes = 6;
+        }
+        else if(abs <= 36028797018963968l){
+            minBytes = 7;
+        }
+        return minBytes;
+    }
+    public static void toBytes(long val, byte[] dest,int position, int numberOfBytes) { //rewrite this to put bytes in a already made array at the right position.
+        for (int i = numberOfBytes - 1; i > 0; i--) {
+            dest[position + i] = (byte) val;
+            val >>= 8;
+        }
+        dest[position] = (byte) val;
+    }
+
+
+    public static long toLong(PageCursor cursor, int offset, int length) {
+        long l = cursor.getByte(offset) < (byte)0 ? -1 : 0;
+        for(int i = offset; i < offset + length; i++) {
+            l <<= 8;
+            l ^= cursor.getByte(i) & 0xFF;
+        }
+        return l;
+    }
+
+
+    private int getLastUsedLeafBufferPosition(){
+        int keyLength = NodeHeader.getKeyLength(dBuffer);
+        int numberOfKeys = NodeHeader.getNumberOfKeys(dBuffer);
+        if(keyLength == 0 || numberOfKeys == 0)
+            return NodeHeader.NODE_HEADER_LENGTH;
+        dBuffer.position(NodeHeader.NODE_HEADER_LENGTH);
+        while(dBuffer.remaining() > keyLength * Long.BYTES){
+            if(dBuffer.getLong() == 0l){
+                dBuffer.position(dBuffer.position() - Long.BYTES);
+                break;
+            }
+            dBuffer.position(dBuffer.position() + (keyLength -1) * Long.BYTES);
+        }
+        return dBuffer.position();
+    }
+
+    private void writeHeaderToCursor(){
+        cursor.setOffset(0);
+        for(int i = 0; i < NodeHeader.NODE_HEADER_LENGTH; i++)
+            cursor.putByte(dBuffer.get(i));
+    }
+
+    private void loadCursorFromDisk(){
+        if (NodeHeader.isUninitializedNode(cursor)) {
+            return;
+        } else if (NodeHeader.isLeafNode(cursor))
+            decompressLeaf();
+        else
+            decompressInternalNode();
+    }
+
+    private void decompressLeaf(){
+        int keyLength = NodeHeader.getKeyLength(cursor);
+        int numberOfKeys = NodeHeader.getNumberOfKeys(cursor);
+        dBuffer.limit(maxPageSize);
+        dBuffer.position(0);
+        cursor.setOffset(0);
+        for(int i = 0; i < NodeHeader.NODE_HEADER_LENGTH; i++){
+            dBuffer.put(cursor.getByte());
+        }
+
+        int position = NodeHeader.NODE_HEADER_LENGTH;
+        int reqBytes;
+        long val;
+        byte header;
+        int firstEncodedIndex;
+        boolean samePath;
+        boolean sameFirstID;
+        long[] prev = new long[keyLength];
+        for(int i = 0; i < numberOfKeys; i++){
+            header = cursor.getByte(position++);
+
+            //
+            firstEncodedIndex = 0;
+            samePath = (sameID & header) == sameID;
+            sameFirstID = (sameFirstNode & header) == sameFirstNode;
+            if(samePath) {
+                firstEncodedIndex++;
+                val = prev[0];
+                dBuffer.putLong(val);
+            }
+            if(sameFirstID) {
+                firstEncodedIndex++;
+                val = prev[1];
+                dBuffer.putLong(val);
+            }
+            header &= ~(1 << 7);
+            header &= ~(1 << 6);
+            reqBytes = header;
+            //
+
+            for(int j = firstEncodedIndex; j < (keyLength); j++){
+                val = prev[j] + toLong(cursor, position, reqBytes);
+                dBuffer.putLong(val);
+                prev[j] = val;
+                position += reqBytes;
+            }
+        }
+        mostRecentCompressedLeafSize = position;
+    }
+
+    private void decompressInternalNode(){
+        Arrays.fill(dBuffer.array(), (byte)0);
+        dBuffer.position(0);
+        cursor.setOffset(0);
+        dBuffer.limit(DiskCache.PAGE_SIZE);
+        for(int i = 0; i < DiskCache.PAGE_SIZE; i++){
+            dBuffer.put(cursor.getByte());
+        }
+    }
+
+    @Override
+    public long getCurrentPageId() {
+        return cursor.getCurrentPageId();
+    }
+
+    @Override
+    public int capacity() {
+        if(NodeHeader.isLeafNode(this))
+            return maxPageSize;
+        else
+            return DiskCache.PAGE_SIZE;
+    }
+
+    @Override
+    public int getSize(){
+        return DiskCache.PAGE_SIZE;
+    }
+
+    @Override
+    public void setOffset(int offset) {
+        dBuffer.position(offset);
+    }
+
+    @Override
+    public int getOffset() {
+        return dBuffer.position();
+    }
+
+    @Override
+    public void getBytes(byte[] dest) {
+        dBuffer.get(dest);
+    }
+
+    @Override
+    public byte getByte(int offset) {
+        return dBuffer.get(offset);
+    }
+
+    @Override
+    public void putBytes(byte[] src) {
+        dBuffer.put(src);
+        pushChangesToDisk();
+    }
+
+    @Override
+    public void putByte(byte val){
+        dBuffer.put(val);
+        pushChangesToDisk();
+    }
+
+    @Override
+    public void putByte(int offset, byte val){
+        dBuffer.put(offset, val);
+        pushChangesToDisk();
+    }
+
+    @Override
+    public long getLong() {
+        return dBuffer.getLong();
+    }
+
+    @Override
+    public long getLong(int offset) {
+        return dBuffer.getLong(offset);
+    }
+
+    @Override
+    public void putLong(long val){
+        dBuffer.putLong(val);
+        pushChangesToDisk();
+    }
+    @Override
+    public void putLong(int offset, long val){
+        dBuffer.putLong(offset, val);
+        pushChangesToDisk();
+    }
+
+    @Override
+    public int getInt() {
+        return dBuffer.getInt();
+    }
+
+    @Override
+    public int getInt(int offset) {
+        return dBuffer.getInt(offset);
+    }
+
+    @Override
+    public void putInt(int val){
+        dBuffer.putInt(val);
+        pushChangesToDisk();
+    }
+    @Override
+    public void putInt(int offset, int val){
+        dBuffer.putInt(offset, val);
+        pushChangesToDisk();
+    }
+    @Override
+    public boolean leafNodeContainsSpaceForNewKey(long[] newKey){
+        //return NodeSize.leafNodeContainsSpaceForNewKey(this, newKey);
+        int magic = 10;
+        return mostRecentCompressedLeafSize + (newKey.length * Long.BYTES) + magic < DiskCache.PAGE_SIZE;
+    }
+
+    @Override
+    public void deferWriting() {
+        deferWriting = true;
+    }
+
+    @Override
+    public void resumeWriting() {
+        deferWriting = false;
+        pushChangesToDisk();//maybe not necessary, and this is maybe redundant.
+    }
+
+    @Override
+    public boolean internalNodeContainsSpaceForNewKeyAndChild(long[] newKey){
+        return NodeSize.internalNodeContainsSpaceForNewKeyAndChild(this, newKey);
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.cursor.close();
+    }
+}
---
+++ src/main/java/PathIndex/KeyImpl.java
@@ -0,0 +1,137 @@
+/**
+ * Copyright (C) 2015-2016 - All rights reserved.
+ * This file is part of the PathDB project which is released under the GPLv3 license.
+ * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl.txt for full license details.
+ * You may use, distribute and modify this code under the terms of the GPLv3 license.
+ */
+
+package PathIndex;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * Implements Comparator for sorting keys.
+ */
+public class KeyImpl implements Comparator<long[]> {
+
+    private Long[] composed_key;
+    private Long[] labelPath;
+    private Long[] nodes;
+    private static KeyImpl comparator;
+
+    public KeyImpl(Long[] labelPath, Long[] nodes){
+        this.labelPath = labelPath;
+        this.nodes = nodes;
+        composed_key = new Long[nodes.length + 1];
+    }
+    public KeyImpl(){
+
+    }
+
+    public static KeyImpl getComparator(){
+        if(comparator == null){
+            comparator = new KeyImpl();
+        }
+        return comparator;
+    }
+
+    public Long[] getLabelPath(){
+        return this.labelPath;
+    }
+
+    public Long[] getNodes(){ return this.nodes;}
+
+    public Long[] getComposedKey(Long pathID){
+        composed_key[0] = pathID;
+        System.arraycopy(this.nodes, 0, composed_key, 1, this.nodes.length);
+        return composed_key;
+    }
+
+    /**
+     * Given two long[]'s, which represent the keys, compare them.
+     * @param a key
+     * @param b key
+     * @return a negative integer, zero, or a positive integer if the first argument is less than, equal to, or greater than the second.
+     */
+    public int compare(Long[] a, Long[] b) {
+
+        if(a.length != b.length){ return a.length - b.length; }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] - b[i] != 0) {
+                return a[i].compareTo(b[i]);
+            }
+        }
+        return 0;
+    }
+    public int compare(long[] a, long[] b) {
+
+        if(a.length != b.length){ return a.length - b.length; }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                return Long.compare(a[i],b[i]);
+            }
+        }
+        return 0;
+    }
+
+
+    public int prefixCompare(Long[] search_key, Long[] key) {
+        long temp;
+        if(search_key.length != key.length){
+            return search_key.length - key.length;
+        }
+        for (int i = 0; i < search_key.length; i++) {
+            temp = search_key[i] - key[i];
+            if (temp != 0) {
+                return search_key[i].compareTo(key[i]);
+            }
+        }
+        return 0;
+    }
+    public int prefixCompare(long[] search_key, long[] key) {
+        for (int i = 0; i < search_key.length; i++) {
+            if (search_key[i] - key[i] != 0) {
+                return (int) (search_key[i] - (key[i]));
+            }
+        }
+        return search_key.length - key.length;
+    }
+
+    /**
+     * Given to keys a, b, determines if a is a valid prefix of b.
+     * @param a prefix to check. Your search key
+     * @param b full key to compare to. Your value in the index.
+     * @return
+     */
+    public boolean validPrefix(Long[] a, Long[] b){
+
+        if((a.length > b.length) || a.length == 0){ //The empty prefix should match nothing. You must specify atleast the path, so give me at least one item.
+            return false;
+        }
+        for(int i = 0; i < a.length; i++){
+            if(!a[i].equals(b[i])){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+    public boolean validPrefix(long[] a, long[] b){
+
+        if((a.length > b.length) || a.length == 0){ //The empty prefix should match nothing. You must specify atleast the path, so give me at least one item.
+            return false;
+        }
+        for(int i = 0; i < a.length; i++){
+            if(!(a[i] == (b[i]))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String toString(Long[] key){
+        return Arrays.toString(key);
+    }
+}
