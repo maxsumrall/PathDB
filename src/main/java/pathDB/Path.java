5aa591105044257d5e20576531ce4015dc762640
Max Sumrall <max@sumrall.nl>
2016-09-27 21:31:16 +0200

    Introduce Paths, objects with Nodes that implement comparable.

--- src/main/java/pathDB/Path.java
+++ src/main/java/pathDB/Path.java
@@ -1,17 +1,70 @@
 package pathDB;
 
 import java.util.List;
+import java.util.Objects;
 
-public class Path
+
+public class Path implements Comparable<Path>
 {
-    final int length;
+    private final int length;
     private List<Node> nodes;
-    private List<Edge> edges;
 
-    public Path( int length, List<Node> nodes, List<Edge> edges )
+    public Path( List<Node> nodes )
     {
-        this.length = length;
+        if ( nodes == null )
+        {
+            throw new IllegalArgumentException( "nodes must not be null" );
+        }
+        this.length = nodes.size();
         this.nodes = nodes;
-        this.edges = edges;
+    }
+
+    @Override
+    public boolean equals( Object o )
+    {
+        if ( this == o )
+        {
+            return true;
+        }
+        if ( o == null || getClass() != o.getClass() )
+        {
+            return false;
+        }
+        Path path = (Path) o;
+        return length == path.length && Objects.equals( nodes, path.nodes );
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash( length, nodes );
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Path{" + "length=" + length + ", nodes=" + nodes + "}\n";
+    }
+
+    @Override
+    public int compareTo( Path o )
+    {
+        if ( this == o )
+        {
+            return 0;
+        }
+        if ( length != o.length )
+        {
+            return length - o.length;
+        }
+        for ( int i = 0; i < length; i++ )
+        {
+            if ( nodes.get( i ).getId() - o.nodes.get( i ).getId() != 0 )
+            {
+                return Long.compare( nodes.get( i ).getId(), o.nodes.get( i ).getId() );
+            }
+        }
+        return 0;
     }
 }
+
