e51d60ec13694812e3611817bf261d053a26d7d5
Giedo Mak <giedomak@gmail.com>
2017-01-05 18:59:57 +0100

    update license headers

--- src/main/java/com/pathdb/pathDB/AbstractPath.java
+++ src/main/java/com/pathdb/pathDB/AbstractPath.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2015-2017 - All rights reserved.
+ * This file is part of the pathdb project which is released under the GPLv3 license.
+ * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl.txt for full license details.
+ * You may use, distribute and modify this code under the terms of the GPLv3 license.
+ */
+
 package com.pathdb.pathDB;
 
 /**
