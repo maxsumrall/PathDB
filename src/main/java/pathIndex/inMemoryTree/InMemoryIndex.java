4bce547478ebb0cb401ed11092c17a1f79448319
Max Sumrall <max.sumrall@neotechnology.com>
2017-01-06 17:13:34 +0100

    In-memory tree based on TreeMap

---
+++ src/main/java/pathIndex/inMemoryTree/InMemoryIndex.java
@@ -0,0 +1,52 @@
+package pathIndex.inMemoryTree;
+
+import pathIndex.AbstractPath;
+import pathIndex.Node;
+import pathIndex.Path;
+import pathIndex.PathIndex;
+import pathIndex.PathPrefix;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+public class InMemoryIndex implements PathIndex
+{
+    private TreeMap<AbstractPath,AbstractPath> treeMap = new TreeMap<>();
+
+    @Override
+    public Iterable<Path> getPaths( PathPrefix pathPrefix ) throws IOException
+    {
+        return treeMap.subMap( pathPrefix, nextIncrementalPath( pathPrefix ) ).values().stream()
+                .map( p -> (Path) p )::iterator;
+    }
+
+    @Override
+    public void insert( Path path )
+    {
+        treeMap.put( path, path );
+    }
+
+    private PathPrefix nextIncrementalPath( PathPrefix pathPrefix )
+    {
+        long pathId = pathPrefix.pathId;
+        int pathLength = pathPrefix.length;
+        List<Node> nodes = new ArrayList<>( pathPrefix.nodes.size() );
+        for ( Node node : pathPrefix.nodes )
+        {
+            nodes.add( new Node( node.getId() ) );
+        }
+
+        if ( pathPrefix.nodes.size() == 0 )
+        {
+            return new PathPrefix( pathId + 1, pathLength, nodes );
+        }
+        else
+        {
+            Node remove = nodes.remove( nodes.size() - 1 );
+            nodes.add( new Node( remove.getId() + 1 ) );
+        }
+        return new PathPrefix( pathId, pathLength, nodes );
+    }
+}
---
+++ src/test/java/pathIndex/inMemoryTree/InMemoryIndexTest.java
@@ -0,0 +1,132 @@
+package pathIndex.inMemoryTree;
+
+import org.junit.Test;
+import pathIndex.Node;
+import pathIndex.Path;
+import pathIndex.PathPrefix;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.junit.Assert.assertEquals;
+
+public class InMemoryIndexTest
+{
+    @Test
+    public void simpleInMemoryTest() throws Exception
+    {
+        // given
+        InMemoryIndex inMemoryIndex = new InMemoryIndex();
+        List<Node> nodes = new ArrayList<>();
+        nodes.add( new Node( 1 ) );
+        nodes.add( new Node( 2 ) );
+        nodes.add( new Node( 3 ) );
+        Path path = new Path( 42, nodes );
+
+        // when
+        inMemoryIndex.insert( path );
+
+        // then
+        Iterable<Path> paths = inMemoryIndex.getPaths( new PathPrefix( 42, 3, emptyList() ) );
+        Path next = paths.iterator().next();
+
+        assertEquals( path, next );
+    }
+
+    @Test
+    public void returnsAllPathsOfSameId() throws Exception
+    {
+        // given
+        InMemoryIndex index = createTestDatabaseA();
+
+        // when
+        Iterable<Path> pathsId0 = index.getPaths( new PathPrefix( 0, 3 ) );
+        Iterable<Path> pathsId42 = index.getPaths( new PathPrefix( 0, 3 ) );
+        Iterable<Path> pathsId56 = index.getPaths( new PathPrefix( 0, 3 ) );
+        Iterable<Path> pathsId99 = index.getPaths( new PathPrefix( 0, 3 ) );
+
+        // then
+        ArrayList<Path> results = new ArrayList<>();
+        pathsId0.forEach( results::add );
+        assertEquals( 100, results.size() );
+
+        results.clear();
+        pathsId42.forEach( results::add );
+        assertEquals( 100, results.size() );
+
+        results.clear();
+        pathsId56.forEach( results::add );
+        assertEquals( 100, results.size() );
+
+        results.clear();
+        pathsId99.forEach( results::add );
+        assertEquals( 100, results.size() );
+    }
+
+    @Test
+    public void returnsAllPathsOfSamePathPrefix() throws Exception
+    {
+        // given
+        InMemoryIndex index = createTestDatabaseB();
+        List<Node> nodes = new ArrayList<>( 1 );
+        nodes.add( new Node( 7 ) );
+
+        // when
+        Iterable<Path> pathsId0 = index.getPaths( new PathPrefix( 0, 3, nodes ) );
+        Iterable<Path> pathsId42 = index.getPaths( new PathPrefix( 0, 3, nodes ) );
+        Iterable<Path> pathsId56 = index.getPaths( new PathPrefix( 0, 3, nodes ) );
+        Iterable<Path> pathsId99 = index.getPaths( new PathPrefix( 0, 3, nodes ) );
+
+        // then
+        ArrayList<Path> results = new ArrayList<>();
+        pathsId0.forEach( results::add );
+        assertEquals( 10, results.size() );
+
+        results.clear();
+        pathsId42.forEach( results::add );
+        assertEquals( 10, results.size() );
+
+        results.clear();
+        pathsId56.forEach( results::add );
+        assertEquals( 10, results.size() );
+
+        results.clear();
+        pathsId99.forEach( results::add );
+        assertEquals( 10, results.size() );
+    }
+
+    public InMemoryIndex createTestDatabaseA()
+    {
+        InMemoryIndex inMemoryIndex = new InMemoryIndex();
+        for ( int i = 0; i < 1000; i++ )
+        {
+            for ( int j = 0; j < 100; j++ )
+            {
+                List<Node> nodes = new ArrayList<>();
+                nodes.add( new Node( j ) );
+                nodes.add( new Node( j ) );
+                nodes.add( new Node( j ) );
+                inMemoryIndex.insert( new Path( i, nodes ) );
+            }
+        }
+        return inMemoryIndex;
+    }
+
+    public InMemoryIndex createTestDatabaseB()
+    {
+        InMemoryIndex inMemoryIndex = new InMemoryIndex();
+        for ( int i = 0; i < 1000; i++ )
+        {
+            for ( int j = 0; j < 100; j++ )
+            {
+                List<Node> nodes = new ArrayList<>();
+                nodes.add( new Node( j % 10 ) );
+                nodes.add( new Node( j ) );
+                nodes.add( new Node( j ) );
+                inMemoryIndex.insert( new Path( i, nodes ) );
+            }
+        }
+        return inMemoryIndex;
+    }
+}
