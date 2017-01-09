6bec18c05b8c3989417ca30f296c867050f16fb1
Max Sumrall <max.sumrall@neotechnology.com>
2017-01-09 08:47:34 +0000

    Use Factory method for building index. Makes sense with disk version.

--- src/main/java/com/pathdb/pathIndex/inMemoryTree/InMemoryIndex.java
+++ src/main/java/com/pathdb/pathIndex/inMemoryTree/InMemoryIndex.java
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2015-2017 - All rights reserved.
+ * This file is part of the pathdb project which is released under the GPLv3 license.
+ * See file LICENSE.txt or go to http://www.gnu.org/licenses/gpl.txt for full license details.
+ * You may use, distribute and modify this code under the terms of the GPLv3 license.
+ */
+
 package com.pathdb.pathIndex.inMemoryTree;
 
 
@@ -14,7 +21,12 @@ import java.util.TreeMap;
 
 public class InMemoryIndex implements PathIndex
 {
-    private TreeMap<AbstractPath,AbstractPath> treeMap = new TreeMap<>();
+    private TreeMap<AbstractPath,AbstractPath> treeMap;
+
+    InMemoryIndex()
+    {
+        treeMap = new TreeMap<>();
+    }
 
     @Override
     public Iterable<Path> getPaths( PathPrefix pathPrefix ) throws IOException
